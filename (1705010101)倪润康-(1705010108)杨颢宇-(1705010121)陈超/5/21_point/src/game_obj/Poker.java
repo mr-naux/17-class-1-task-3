9ab3eff093f4a69bbdf474da7f177cf7f0e04420
Cocoabeans <yznrk1999@gmail.com>
2019-10-08 20:45:57 +0800

    Update

---
+++ (1705010101)倪润康-(1705010108)杨颢宇-(1705010121)陈超/5/21_point/src/game_obj/Poker.java
@@ -0,0 +1,20 @@
+package game_obj;
+
+public class Poker {
+	public int point;
+	public String color;
+	public String paimian;
+	public int id;
+	public void getPoint(int _point) {
+		point=_point;
+	}
+	public void getId(int _id) {
+		id=_id;
+	}
+	public void getColor(String _color) {
+		color=_color;
+	}
+	public void getPaimian(String _paimian) {
+		paimian=_paimian;
+	}
+}
